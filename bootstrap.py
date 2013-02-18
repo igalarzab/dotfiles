d6cecd1756c418a2f763bdbbc35f4c905526c7e0
Jose Ignacio Galarza <igalarzab@gmail.com>
2013-02-18 22:15:57 +0100

    Bootstrap file

---
+++ bootstrap.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+#
+# Bootstrap script
+# ===========================================
+#
+#  Jose Ignacio Galarza <igalarzab@gmail.com>
+#
+
+import glob
+import os
+
+
+# Path to this file
+SELF_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def create_symlinks():
+    'Create the symlinks on user home'
+    symlinks = glob.glob('*/*.symlink')
+    always_overwrite = False
+
+    for symlink in symlinks:
+	origin = os.path.join(SELF_PATH, symlink)
+
+        filename = '.' + os.path.splitext(os.path.basename(symlink))[0]
+        destine = os.path.expanduser(os.path.join('~', filename))
+
+        if os.path.lexists(destine):
+
+            # Ask the user if overwrite the file
+            if not always_overwrite:
+                answer = confirm_overwrite(destine)
+                always_overwrite = answer == 'a'
+
+            # Remove the file if the user answered yes
+            if always_overwrite or answer in ('y', 'a'):
+                os.remove(destine)
+
+            # Keep the file if the user answered no
+            if answer == 'n':
+                continue
+
+        # Create the symlink
+        os.symlink(origin, destine)
+
+
+def confirm_overwrite(path):
+    'Ask the user to overwrite a file'
+    while True:
+        answer = raw_input('%s exists, overwrite it? (a/y/n) ' %
+                           os.path.basename(path))
+
+        if answer in ('y', 'n', 'a'):
+            break
+
+    return answer
+
+
+if __name__ == '__main__':
+    os.chdir(SELF_PATH)
+    create_symlinks()
