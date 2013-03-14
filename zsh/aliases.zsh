a8e52dff5cbd6e179fb38e3056a995f8aec2f98d
Jose Ignacio Galarza <igalarzab@gmail.com>
2013-03-14 02:24:05 +0100

    Chef configuration

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,13 +10,18 @@ then
   alias sed="gsed"
 fi
 
+# ls aliases
 alias l="ls"
 alias ll="ls -l"
 alias la="ls -A"
 alias lla="ls -lA"
 
+# Generic aliases
 alias grep="grep --color=auto"
 alias reload='. ~/.zshrc'
 
 # cd into Development folder
 alias d="cd $PROJECTS"
+
+# Fucking auto-complete :D
+alias knife="nocorrect knife"
