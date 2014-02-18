df941be91055e408e07eb64f4af86320697ac4aa
Jose Ignacio Galarza <igalarzab@gmail.com>
2014-02-18 10:29:17 +0100

    Changes to the PATH

--- go/env.zsh
+++ go/env.zsh
@@ -1,3 +1,3 @@
-export GOPATH=$GOPATH:~/.golang
+export GOPATH=~/.golang
 export GOROOT=`go env GOROOT`
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -29,6 +29,7 @@ alias reload='. ~/.zshrc'
 # cd into Development folder
 alias d="cd $COMPANY"
 alias s="cd $PROJECTS"
+alias m="cd ~/Code/mercabitcoin"
 
 # Fucking auto-complete :D
 alias knife="nocorrect knife"
