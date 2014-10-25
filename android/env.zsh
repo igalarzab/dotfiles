5cfb41912bc24d1dea1c07751d1a00234baab1d9
Jose Ignacio Galarza <igalarzab@gmail.com>
2014-10-25 09:51:05 +0200

    Set JAVA_HOME for Android

--- android/env.zsh
+++ android/env.zsh
@@ -3,6 +3,8 @@
 # Will look for the android-sdk installation in HomeBrew
 #
 
+JAVA_HOME=`/usr/libexec/java_home -v 1.7`
+
 if (( $+commands[brew] ))
 then
     export ANDROID_HOME="`brew --prefix android-sdk`"
