94cd4c6052e8b799059b92dafdcac51f39a3de9f
Jose Galarza <igalarzab@gmail.com>
2017-08-12 17:20:19 +0100

    Kubernetes shortcuts

--- kubernetes/env.zsh
+++ kubernetes/env.zsh
@@ -1,1 +1,36 @@
-alias kubesys=kubectl -n kube-system
+alias kc=kubectl
+
+kcs() {
+    kubectl --namespace=kube-system $@
+}
+
+kclogs() {
+    if [ -z $1 ]; then
+        echo "Usage: kclogs pod-exp [pod-number]"
+        return
+    fi
+
+    line=1
+
+    if [ ! -z $2 ]; then
+        line=$2
+    fi
+
+    kubectl logs --tail 100 --follow `kubectl get pods | grep $1 | awk NR==${line}'{print $1}'`
+}
+
+kcbash() {
+    if [ -z $1 ]; then
+        echo "Usage: kcbash pod-exp"
+        return
+    fi
+
+    line=1
+
+    if [ ! -z $2 ]; then
+        line=$2
+    fi
+
+    kubectl exec -ti `kubectl get pods | grep $1 | awk NR==${line}'{print $1}'` /bin/bash
+}
+
