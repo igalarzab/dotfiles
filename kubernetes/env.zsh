928fb5d3a8c7909803e2908359f8ba1ab58cb82a
Jose Galarza <igalarzab@gmail.com>
2018-03-02 00:05:36 +0000

    New packages

--- kubernetes/env.zsh
+++ kubernetes/env.zsh
@@ -1,36 +1,14 @@
 alias kc=kubectl
 
+if [ $commands[stern] ]; then
+  source <(stern --completion=zsh)
+
+  klogs () {
+    stern --all-namespaces --since 10m --tail 10 $@
+  }
+fi
+
+
 kcs() {
     kubectl --namespace=kube-system $@
 }
-
-kclogs() {
-    if [ -z $1 ]; then
-        echo "Usage: kclogs pod-exp [pod-number]"
-        return
-    fi
-
-    line=1
-
-    if [ ! -z $2 ]; then
-        line=$2
-    fi
-
-    kubectl logs --tail 100 --follow `kubectl get pods | grep $1 | awk NR==${line}'{print $1}'`
-}
-
-kcbash() {
-    if [ -z $1 ]; then
-        echo "Usage: kcbash pod-exp"
-        return
-    fi
-
-    line=1
-
-    if [ ! -z $2 ]; then
-        line=$2
-    fi
-
-    kubectl exec -ti `kubectl get pods | grep $1 | awk NR==${line}'{print $1}'` /bin/bash
-}
-
