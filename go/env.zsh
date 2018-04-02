98d70d69aacc9e134e71dc58a8e842871c684420
Jose Galarza <igalarzab@gmail.com>
2018-04-02 20:14:10 +0200

    Fix PATH

--- go/env.zsh
+++ go/env.zsh
@@ -2,5 +2,5 @@ if (( $+commands[go] ))
 then
     export GOROOT=`go env GOROOT`
     export GOPATH=~/.golang/global
-    export PATH=$PATH:/usr/local/opt/go/libexec/bin:$GOPATH/bin
+    export PATH=$PATH:$GOPATH/bin
 fi
