b6e43e8dacb98bbee45a7d5a5b8d45abf4a3f9b4
Jose Galarza <igalarzab@gmail.com>
2018-04-02 20:14:44 +0200

    Start using pyenv and pipenv

--- python/env.zsh
+++ python/env.zsh
@@ -1,3 +1,13 @@
-# virtualenv
-export WORKON_HOME=~/.virtualenvs
-export VIRTUAL_ENV_DISABLE_PROMPT=1
+if (( $+commands[pyenv] ))
+then
+    export PYENV_ROOT=~/.pyenv/
+    export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+fi
+
+if (( $+commands[pipenv] ))
+then
+    export PIPENV_SHELL_COMPAT=1
+    export PIPENV_SHELL_FANCY=1
+fi
