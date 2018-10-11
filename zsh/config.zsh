3d36eac4da69f0b4c592ef7419769ef20fe06962
Jose Galarza <igalarzab@gmail.com>
2018-10-11 14:47:20 +0900

    Update zsh config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,6 @@
+# Add each topic folder to fpath so that they can add functions and completion scripts
+for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
@@ -25,4 +28,23 @@ setopt HIST_REDUCE_BLANKS
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
+
+# Beginning and enf of line with Fn+L or Fn+R
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+
+# Better history
+# Credits to https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
+
+# Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
+bindkey '^?' backward-delete-char
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
+bindkey "\e[3~" delete-char
 
