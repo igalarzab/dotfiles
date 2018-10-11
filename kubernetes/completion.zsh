bf8b4650b20a740b3589d49578689abc6bd52557
Jose Galarza <igalarzab@gmail.com>
2018-10-11 14:53:37 +0900

    Add kubernetes context to the iterm2 badge

--- kubernetes/completion.zsh
+++ kubernetes/completion.zsh
@@ -1,3 +1,10 @@
 if (( $+commands[stern] )); then
   source <(stern --completion=zsh)
 fi
+
+# Set the iterm2 badge
+if [[ -z "${ITERM_SHELL_INTEGRATION}" ]]; then
+    if (( $+commands[kubectl] )); then
+        iterm2_set_user_var kubeContext $(kubectl config current-context)
+    fi
+fi
--- osx/iterm2.zsh
+++ osx/iterm2.zsh
@@ -1,2 +1,5 @@
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+if [ -f "${HOME}/.iterm2_shell_integration.zsh" ]; then
+    export ITERM_APP=1
+    source "${HOME}/.iterm2_shell_integration.zsh"
+fi
 
