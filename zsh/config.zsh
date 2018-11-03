112db7eef5647313362940e406b9fa9562d3917e
Jose Galarza <igalarzab@gmail.com>
2018-11-03 13:14:47 +0000

    Fix search inz sh

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,12 +1,9 @@
-# Add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
-
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=1000000
+SAVEHIST=1000000
 
 setopt NO_BG_NICE
 setopt NO_HUP
@@ -28,23 +25,3 @@ setopt HIST_REDUCE_BLANKS
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
-
-# Beginning and enf of line with Fn+L or Fn+R
-bindkey "^[[H" beginning-of-line
-bindkey "^[[F" end-of-line
-
-# Better history
-# Credits to https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
-autoload -U up-line-or-beginning-search
-autoload -U down-line-or-beginning-search
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search # Up
-bindkey "^[[B" down-line-or-beginning-search # Down
-
-# Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
-bindkey '^?' backward-delete-char
-bindkey "^[[3~" delete-char
-bindkey "^[3;5~" delete-char
-bindkey "\e[3~" delete-char
-
