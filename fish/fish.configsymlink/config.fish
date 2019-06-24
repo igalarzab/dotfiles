ae62a378a68b8e19e4785139f424878d4b595af2
Jose Galarza <igalarzab@gmail.com>
2019-06-24 17:19:16 +0100

    Install fisher and dependencies

--- fish/fish.configsymlink/config.fish
+++ fish/fish.configsymlink/config.fish
@@ -9,7 +9,12 @@ set -g fisher_path ~/.config/fish/fisher/
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
 set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
 
+# Install fisher and packages
+if not functions -q fisher
+    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 for file in $fisher_path/conf.d/*.fish
     builtin source $file 2> /dev/null
 end
-
