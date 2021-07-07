be3c9076a0c49cef6afad27cc104d86070d25d38
Jose Galarza <igalarzab@gmail.com>
2021-07-07 11:57:15 +0900

    lint

--- vim/nvim.configsymlink/init.lua
+++ vim/nvim.configsymlink/init.lua
@@ -27,6 +27,7 @@ require('packer').startup(function()
 
     -- NeoVim helpers
     use('neovim/nvim-lspconfig')
+    use('glepnir/lspsaga.nvim')
     use({'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'})
 
     -- Auto-Completion
@@ -107,7 +108,7 @@ opt.whichwrap:append('<,>,h,l,[,]')     -- Move between lines with the arrows
 
 
 ---------------------------------------------------------------------------
--- Plugins Config 
+-- Plugins Config
 ---------------------------------------------------------------------------
 
 -- Auto Pairs
@@ -139,10 +140,17 @@ require('gitsigns').setup()
 -- LuaLine
 require('lualine').setup({
     options = {
-        theme = 'onedark'
+        icons_enabled = true,
+        theme = 'onedark',
     },
     sections = {
-        lualine_x = {'filetype'},
+        lualine_b = {
+            'branch',
+        },
+        lualine_x = {
+            { 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
+            'filetype',
+        },
     },
 })
 
@@ -155,20 +163,49 @@ nlsp.pyright.setup({})
 nlsp.stylelint_lsp.setup({})
 nlsp.yamlls.setup({})
 
+-- LSP - Formatting and auto-save
+api.nvim_set_keymap("n", "<Leader>ff", ":lua vim.lsp.buf.formatting()<CR>", { noremap = true, silent = true })
+vim.cmd [[autocmd BufWritePre *.go lua vim.lsp.buf.formatting_sync()]]
+
+-- LSP Saga
+require('lspsaga').init_lsp_saga({
+    error_sign = '',
+    warn_sign = '',
+    hint_sign = '',
+    infor_sign = '',
+    border_style = 'round',
+    finder_action_keys = {
+        open = '<CR>', vsplit = '<C-v>', split = '<C-h>', quit = '<ESC>',
+        scroll_down = '<C-f>', scroll_up = '<C-b>'
+    },
+    code_action_keys = {
+        quit = '<ESC>', exec = '<CR>'
+    },
+    rename_action_keys = {
+        quit = '<ESC>', exec = '<CR>'
+    },
+})
+
+api.nvim_set_keymap('n', '<Leader>gr', ':Lspsaga lsp_finder<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<Leader>gs', ':Lspsaga signature_help<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<Leader>gd', ':Lspsaga hover_doc<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<Leader>ga', ':Lspsaga code_action<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<Leader>ge', ':Lspsaga diagnostic_jump_next<CR>', { noremap = true, silent = true })
+
 -- OneDark
 require('onedark').setup()
 
 -- Telescope
-api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true })
-api.nvim_set_keymap('n', '<C-g>', ':Telescope live_grep<CR>', { noremap = true })
+api.nvim_set_keymap('n', '<C-p>', ':Telescope find_files<CR>', { noremap = true, silent = true })
+api.nvim_set_keymap('n', '<C-g>', ':Telescope live_grep<CR>', { noremap = true, silent = true })
 
 -- Tree Browser
-api.nvim_set_keymap('n', '<Leader>tr', ':NvimTreeToggle<CR>', { noremap = true })
 g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
 g.nvim_tree_gitignore = 1
 g.nvim_tree_group_empty = 1
 g.nvim_tree_special_files = {}
 g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1, folder_arrows = 1}
+api.nvim_set_keymap('n', '<Leader>tr', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 -- TreeSitter config
 require('nvim-treesitter.configs').setup({
