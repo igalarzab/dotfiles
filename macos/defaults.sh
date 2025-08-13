f0a9773111c6a6ec858f9ffb8aa61b61cd19eb23
Jose Galarza <igalarzab@gmail.com>
2025-08-13 11:25:48 +0800

    Some macOS defaults

---
+++ macos/defaults.sh
@@ -0,0 +1,35 @@
+#
+# MacOS Defaults
+#
+# https://macos-defaults.com/
+#
+#
+
+# Dock
+defaults write com.apple.dock "orientation" -string "left"
+defaults write com.apple.dock "tilesize" -int "30"
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock "show-recents" -bool "false"
+
+# Finder
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+defaults write com.apple.finder "ShowPathbar" -bool "true"
+defaults write com.apple.finder "FXPreferredViewStyle" -string "clmv"
+defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
+defaults write com.apple.finder "ShowExternalHardDrivesOnDesktop" -bool "true"
+
+# Keyboard
+defaults write NSGlobalDomain "ApplePressAndHoldEnabled" -bool "false"
+
+# Screenshots
+defaults write com.apple.screencapture "location" -string "~/Pictures"
+
+# TextEdit
+defaults write com.apple.TextEdit "RichText" -bool "false"
+defaults write com.apple.TextEdit "SmartQuotes" -bool "false"
+
+# TimeMachine
+defaults write com.apple.TimeMachine "DoNotOfferNewDisksForBackup" -bool "true"
+
+# Restart services
+killall Dock Finder SystemUIServer TextEdit
